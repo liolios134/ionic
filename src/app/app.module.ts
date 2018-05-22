d684ee6601130032acfce8784bb6b4724dc3e867
Yaconnan the Barbarian <liolios134@gmail.com>
2018-05-22 12:39:18 +0300

    adding files

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { TodoProvider } from '../providers/todo/todo';
+import { HttpClientModule } from "@angular/common/http";
+import { ArchivedTodosPage } from "../pages/archived-todos/archived-todos";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    ArchivedTodosPage
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    ArchivedTodosPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoProvider
+  ]
+})
+export class AppModule {}
